79e5931ceaeaca9ac4459bebd657c812f5ae68bc
Vitalii <vitalik.ilkiv26@gmail.com>
2026-02-01 15:14:26 +0200

    change burger

--- header.php
+++ header.php
@@ -61,21 +61,7 @@
       <div class="container">
         <div class="header__wrap">
            <button class="header__burger j-burger-action hidden-tablet-l" aria-label="Відкрити меню">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            >
-              <path d="M4 6 H20"></path>
-              <path d="M4 12 H20"></path>
-              <path d="M4 18 H20"></path>
-            </svg>
+            <span></span>
           </button>
           <div class="header__logo">
             <a href="">
